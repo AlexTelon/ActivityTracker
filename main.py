e69bc20da0e31f2fc9515fa7691f40b82ae59bbb
Alex Telon <alex.telon@gmail.com>
2023-03-18 12:17:03 +0100

    Trying stuff. Adding patterns.ini

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import os
 import time
 import csv
 import datetime
+import re
+import win32gui
+import configparser
 
-if os.name == 'nt':
-    import win32gui
-else:
-    quit('Only Windows is supported at the moment.')
-
-def get_active_window_title_windows():
+def get_active_window_title_windows(patterns):
     hwnd = win32gui.GetForegroundWindow()
-    return win32gui.GetWindowText(hwnd)
+    window_title = win32gui.GetWindowText(hwnd)
+    title = ' '.join(window_title.strip().split())
+    print(title)
+    # Match window title against predefined patterns
+    for app, pattern in patterns.items():
+        if re.match(pattern, title):
+            return app
+
+    # If no match is found, return 'Other'
+    return "Other"
+
+# Read patterns from a config file
+config = configparser.ConfigParser()
+config.read("patterns.ini")
+patterns = dict(config["patterns"])
 
 # Initialize variables
 current_app = ""
-start_time = datetime.datetime.now()
 data_file = "time_tracking_data.csv"
 
 while True:
     # Get the current active window's title
-    window_title = get_active_window_title_windows()
-
+    window_title = get_active_window_title_windows(patterns)
+    print(window_title)
     # Check if the application has changed
     if window_title != current_app:
-        # Record the previous application's usage time
-        end_time = datetime.datetime.now()
-        duration = (end_time - start_time).total_seconds()
+        # Record the timestamp and application
+        timestamp = datetime.datetime.now()
 
-        # Save the data to the CSV file
-        with open(data_file, "a", newline='', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerow([current_app, start_time, end_time, duration])
+        # # Save the data to the CSV file
+        # with open(data_file, "a", newline='', encoding='utf-8') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow([current_app, timestamp])
 
-        # Update the current application and start time
+        # Update the current application
         current_app = window_title
-        start_time = datetime.datetime.now()
 
-    # Poll every second
+    # Poll every x seconds
     time.sleep(1)
