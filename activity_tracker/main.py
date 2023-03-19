04c4395330542f64ec625899226de319f7a1e1e7
Alex Telon <alex.telon@gmail.com>
2023-03-19 13:03:25 +0100

    Writing offline when shutting down tracker

--- activity_tracker/main.py
+++ activity_tracker/main.py
@@ -14,7 +14,10 @@ def get_idle_time():
     return (current_time - last_input) / 1000  # Convert milliseconds to seconds
 
 def exit_handler(signal, frame):
-    # Removes the KeyboardInterupt exception text from the console for instance.
+    timestamp = int(time.time())
+    cursor.execute("INSERT INTO window_data (timestamp, window_name) VALUES (?, ?)", (timestamp, "offline"))
+    conn.commit()
+    conn.close()
     exit(0)
 
 # Catch signals (Ctrl-C and others)
@@ -24,31 +27,31 @@ signal.signal(signal.SIGTERM, exit_handler)
 with open('patterns.json', 'r') as file:
     patterns = json.load(file)
 
-with sqlite3.connect('time_tracking_data.sqlite') as conn:
-    cursor = conn.cursor()
+conn = sqlite3.connect('time_tracking_data.sqlite')
+cursor = conn.cursor()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS window_data
-                      (timestamp INTEGER, window_name TEXT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS window_data
+                  (timestamp INTEGER, window_name TEXT)''')
 
-    while True:
-        idle_time = get_idle_time()
-        if idle_time > IDLE_THRESHOLD:
-            window_name = "idle"
-        else:
-            hwnd = win32gui.GetForegroundWindow()
-            if win32gui.IsWindowVisible(hwnd):
-                class_name = win32gui.GetClassName(hwnd)
-                title = win32gui.GetWindowText(hwnd)
+while True:
+    idle_time = get_idle_time()
+    if idle_time > IDLE_THRESHOLD:
+        window_name = "idle"
+    else:
+        hwnd = win32gui.GetForegroundWindow()
+        if win32gui.IsWindowVisible(hwnd):
+            class_name = win32gui.GetClassName(hwnd)
+            title = win32gui.GetWindowText(hwnd)
 
-                window_name = find_window_name(class_name, title, patterns)
+            window_name = find_window_name(class_name, title, patterns)
 
-        if not window_name:
-            print('No match for window: %s - %s' % (class_name, title))
+    if not window_name:
+        print('No match for window: %s - %s' % (class_name, title))
 
-        timestamp = int(time.time())
-        cursor.execute("INSERT INTO window_data (timestamp, window_name) VALUES (?, ?)", (timestamp, window_name or 'other'))
-        conn.commit()
+    timestamp = int(time.time())
+    cursor.execute("INSERT INTO window_data (timestamp, window_name) VALUES (?, ?)", (timestamp, window_name or 'other'))
+    conn.commit()
 
-        print(f"[{timestamp}] {window_name}")
+    print(f"[{timestamp}] {window_name}")
 
-        time.sleep(10)
+    time.sleep(10)
