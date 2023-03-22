4c97828c357ed9ee91e02be6d2bd90be9a082172
Alex Telon <alex.telon@gmail.com>
2023-03-22 22:20:01 +0100

    Added toolbar with zoom etc to calendar plot

--- activity_tracker/tkinter_viz.py
+++ activity_tracker/tkinter_viz.py
@@ -1,4 +1,4 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from tkinter import *
 from tkcalendar import *
 from database import read_data_from_date
@@ -28,10 +28,6 @@ def display_activity_data(event):
     # Generate the daily schedule chart
     fig = generate_daily_schedule_chart(data, date)
 
-    # Clear the previous plot, if any
-    if plot_widget is not None:
-        plot_widget.grid_forget()
-
     # Create a FigureCanvasTkAgg object from the generated plot
     canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.draw()
@@ -39,6 +35,14 @@ def display_activity_data(event):
     # Create a tkinter canvas to display the plot
     plot_widget = canvas.get_tk_widget()
     plot_widget.grid(row=0, column=1, padx=10, pady=10, sticky=N+S+E+W)
+
+    # Add a navigation toolbar to the plot widget
+    toolbar_frame = Frame(frame)
+    toolbar_frame.grid(row=1, column=1, padx=10, pady=10, sticky=E)
+
+    toolbar = NavigationToolbar2Tk(canvas, toolbar_frame)
+    toolbar.update()
+    toolbar.grid(row=1, column=1, padx=10, pady=10, sticky=E)
 
 # Bind the display_activity_data function to the <<CalendarSelected>> event
 cal.bind("<<CalendarSelected>>", display_activity_data)
